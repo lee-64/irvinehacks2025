340dcc8c7f48297f861c0174455c715325d53da1
Daniel Yang <danielyangdev@gmail.com>
2025-01-25 20:52:08 -0800

    merge fix

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import InteractiveMap from '@/components/InteractiveMap';
 import MapSearch from '@/components/MapSearch';
