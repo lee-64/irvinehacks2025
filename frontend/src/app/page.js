cba5ee23187400aafde66d627ebde087d144b70d
Lee Stilwell <lstilwel@usc.edu>
2025-01-26 02:48:19 -0800

    working demo; however card sizing incorrect

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -147,158 +147,151 @@ export default function Home() {
   };
 
   return (
-      <div className="sm:p-10">
-        <div className="absolute top-4 right-4">
-          <Link href="/compare">
-            <button className="bg-blue-600 text-white py-2 px-4 rounded-lg shadow">
-              Compare Locations
-            </button>
-          </Link>
-        </div>
+    <div className="sm:p-10">
+      <div className="absolute top-4 right-4">
+        <Link href="/compare">
+          <button className="bg-blue-600 text-white py-2 px-4 rounded-lg shadow">
+            Compare Locations
+          </button>
+        </Link>
+      </div>
 
-        <div className="h-20 row-start-1 items-center">
-          <img src="/zipscopeimage.png" className="h-full"/>
-        </div>
+      <div className="h-20 row-start-1 items-center">
+        <img src="/zipscopeimage.png" className="h-full" />
+      </div>
 
-        <div className="h-screen flex items-center flex-row">
-          <div className="basis-1/2 text-center sm:p-10">
-            <h1 className="text-4xl lg:text-5xl font-bold text-gray-900 leading-snug">
-              Looking for your <br/>
-              next adventure or <br/>
-              place to call home?
-            </h1>
-            <p className="text-blue-600 text-lg font-semibold mt-4 inline-block">Explore ZipScope</p>
-            <p className="text-gray-600 mt-4">
-              Your SoCal neighborhood guide – for travelers, new home seekers, and more. Get comprehensive data insights
-              into the best areas in the LA/OC area.
-            </p>
-          </div>
-          <div className="basis-1/2 text-center flex justify-center items-center w-full">
-            <div className="h-96 w-96 rounded-lg">
-              <img src="/skyline.png" className="h-auto rounded-lg"/>
-            </div>
-          </div>
+      <div className="h-screen flex items-center flex-row">
+        <div className="basis-1/2 text-center sm:p-10">
+          <h1 className="text-4xl lg:text-5xl font-bold text-gray-900 leading-snug">
+            Looking for your <br />
+            next adventure or <br />
+            place to call home?
+          </h1>
+          <p className="text-blue-600 text-lg font-semibold mt-4 inline-block">Explore ZipScope</p>
+          <p className="text-gray-600 mt-4">
+            Your SoCal neighborhood guide – for travelers, new home seekers, and more. Get comprehensive data insights into the best areas in the LA/OC area.
+          </p>
         </div>
-
-        <div
-            className="py-16 bg-gradient-to-r from-gray-100 via-white to-gray-100"
-            style={{
-              opacity: opacity(scrollAmount),
-            }}
-        >
-          <div className="max-w-4xl mx-auto text-center">
-            <h1 className="text-4xl font-bold text-gray-800 mb-8">Based on the following metrics...</h1>
-            <div className="flex justify-center gap-4">
-              {[cards[currentSet], cards[(currentSet + 1) % cards.length], cards[(currentSet + 2) % cards.length]].map(
-                  (card, index) => (
-                      <div
-                          key={index}
-                          className="bg-white rounded-xl shadow-lg p-3 w-64 transform transition-transform duration-300 hover:scale-105"
-                      >
-                        <div className="w-full h-64">
-                          <img
-                              src={card.imgSrc}
-                              alt={card.title}
-                              className="w-full h-full object-cover rounded-lg"
-                          />
-                        </div>
-                        <h2 className="text-lg font-semibold text-gray-700 mt-3">{card.title}</h2>
-                        <p className="mt-2 text-sm text-gray-500">{card.description}</p>
-                      </div>
-                  )
-              )}
-            </div>
-            <div className="flex justify-center mt-6 space-x-2">
-              {cards.map((_, index) => (
-                  <div
-                      key={index}
-                      className={`h-2 w-2 rounded-full ${
-                          index === currentSet ? 'bg-blue-600' : 'bg-gray-300'
-                      }`}
-                  ></div>
-              ))}
-            </div>
+        <div className="basis-1/2 text-center flex justify-center items-center w-full">
+          <div className="h-96 w-96 rounded-lg">
+            <img src="/skyline.png" className="h-auto rounded-lg" />
           </div>
-        </div>
-
-        <div className="min-h-screen" style={{display: 'flex'}}>
-          <div className="min-h-screen" style={{flex: '50%'}}>
-            <div className="min-h-screen w-full sm:mr-20" style={{marginRight: '90px'}}>
-              <MapSearch
-                  onSubmit={handleMapSearch}
-                  placeholder="568 N Tigertail Rd, Los Angeles"
-              />
-              <ErrorAlert
-                  message={error}
-              />
-
-              {status === "success" && !loading && (
-                  <div className="flex flex-col items-center justify-center gap-4 mt-8">
-                    <div style={{width: '80%'}}>
-                      <br/><br/><br/><p className="col-span-2">{locData.explanation}</p><br/><br/>
-                      <p className="col-span-2">Things to do nearby: {locData.tourism_highlights}</p>
-                    </div>
-                    <div className="h-80" style={{display: 'flex'}}>
-                      <div className="w-full sm:mr-20" style={{flex: '50%'}}>
-                        {renderPieChart(locData.safety_score, "Safety", locData.safety_score > 50)}
-                      </div>
-                      <div className="w-full sm:mr-20" style={{flex: '50%'}}>
-                        {renderPieChart(locData.environmental_score, "Pollution", locData.environmental_score > 50)}
-                      </div>
-                    </div>
-                    <div className="h-80" style={{display: 'flex'}}>
-                      <div className="w-full sm:mr-20" style={{flex: '50%'}}>
-                        {renderPieChart(locData.health_score, "Health", locData.health_score > 50)}
-                      </div>
-                      <div className="w-full sm:mr-20" style={{flex: '50%'}}>
-                        {renderPieChart(locData.school_score, "Education", locData.school_score > 50)}
-                      </div>
-                    </div>
-                    <div className="h-80" style={{display: 'flex'}}>
-                      <div className="w-full sm:mr-20" style={{flex: '50%'}}>
-                        {renderPieChart(locData.walk_score, "Transportation", locData.walk_score > 50)}
-                      </div>
-                      <div className="w-full sm:mr-20" style={{flex: '50%'}}>
-                        {renderPieChart(locData.housing_score, "Housing", locData.housing_score > 50)}
-                      </div>
-                    </div>
-                  </div>
-              )}
-            </div>
-          </div>
-          <div className="column flex-1" style={{flex: '50%'}}>
-            <div className="min-h-screen w-full">
-              <InteractiveMap
-                  latitude={locData.lat}
-                  longitude={locData.lon}
-                  desirability={locData.overall_score}
-              />
-            </div>
-          </div>
-          {/*{loading && (*/}
-          {/*)}*/}
-        </div>
-        <div style={{height: '500px', width: '100%', backgroundColor: 'white'}}></div>
-        <div
-            style={{
-              height: "50px",
-              backgroundColor: "white",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              opacity: isOverscrolled ? 1 : 0,
-              transform: isOverscrolled ? "scale(1)" : "scale(0.8)",
-              transition: "all 0.5s ease-in-out",
-              position: "fixed",
-              bottom: 150,
-              left: 0,
-              right: 0,
-              zIndex: 10
-            }}
-        >
-          < img src="/sunshine.png" className="fixed bottom-0 left-0 h-96 z-10 pointer-events-none"
-          />
         </div>
       </div>
+
+      <div
+        className="py-16 bg-gradient-to-r from-gray-100 via-white to-gray-100"
+        style={{
+          opacity: opacity(scrollAmount),
+        }}
+      >
+        <div className="max-w-6xl mx-auto text-center">
+          <h1 className="text-4xl font-bold text-gray-800 mb-8">Based on the following metrics...</h1>
+          <div className="flex items-center justify-center space-x-6">
+            {[cards[currentSet], cards[(currentSet + 1) % cards.length], cards[(currentSet + 2) % cards.length]].map(
+              (card, index) => (
+                <div
+  key={index}
+  className="bg-white rounded-xl shadow-lg p-4 w-48 h-48 transform transition-transform duration-300 hover:scale-105"
+>
+                  <img src={card.imgSrc} alt={card.title} className="w-full h-28 object-cover rounded-lg mb-4" />
+                  <h2 className="text-xl font-semibold text-gray-700">{card.title}</h2>
+                  <p className="mt-2 text-gray-500">{card.description}</p>
+                </div>
+              )
+            )}
+          </div>
+          <div className="flex justify-center mt-6 space-x-2">
+            {cards.map((_, index) => (
+              <div
+                key={index}
+                className={`h-2 w-2 rounded-full ${
+                  index === currentSet ? 'bg-blue-600' : 'bg-gray-300'
+                }`}
+              ></div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+    <div className="min-h-screen" style={{display: 'flex'}}>
+      <div className="min-h-screen" style={{flex: '50%'}}>
+        <div className="min-h-screen w-full sm:mr-20" style={{ marginRight: '90px' }}>
+          <MapSearch
+              onSubmit={handleMapSearch}
+              placeholder="568 N Tigertail Rd, Los Angeles"
+          />
+          <ErrorAlert
+              message={error}
+          />
+
+          {status === "success" && !loading && (
+              <div className="flex flex-col items-center justify-center gap-4 mt-8">
+                <div style={{width: '80%'}}>
+                  <br/><br/><br/><p className="col-span-2">{locData.explanation}</p><br/><br/>
+                  <p className="col-span-2">Things to do nearby: {locData.tourism_highlights}</p>
+                </div>
+                <div className="h-80" style={{display: 'flex'}}>
+                  <div className="w-full sm:mr-20" style={{flex: '50%'}}>
+                    {renderPieChart(locData.safety_score, "Safety", locData.safety_score > 50)}
+                  </div>
+                  <div className="w-full sm:mr-20" style={{flex: '50%'}}>
+                    {renderPieChart(locData.environmental_score, "Pollution", locData.environmental_score > 50)}
+                  </div>
+                </div>
+                <div className="h-80" style={{display: 'flex'}}>
+                  <div className="w-full sm:mr-20" style={{flex: '50%'}}>
+                    {renderPieChart(locData.health_score, "Health", locData.health_score > 50)}
+                  </div>
+                  <div className="w-full sm:mr-20" style={{flex: '50%'}}>
+                    {renderPieChart(locData.school_score, "Education", locData.school_score > 50)}
+                  </div>
+                </div>
+                <div className="h-80" style={{display: 'flex'}}>
+                  <div className="w-full sm:mr-20" style={{flex: '50%'}}>
+                    {renderPieChart(locData.walk_score, "Transportation", locData.walk_score > 50)}
+                  </div>
+                  <div className="w-full sm:mr-20" style={{flex: '50%'}}>
+                    {renderPieChart(locData.housing_score, "Housing", locData.housing_score > 50)}
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="column flex-1" style={{ flex: '50%' }}>
+          <div className="min-h-screen w-full">
+            <InteractiveMap
+              latitude={locData.lat}
+              longitude={locData.lon}
+              desirability={locData.overall_score}
+            />
+          </div>
+        </div>
+        {/*{loading && (*/}
+        {/*)}*/}
+      </div>
+      <div style = {{height: '500px', width: '100%', backgroundColor: 'white'}} > </div>
+        <div
+          style = {{
+          height: "50px",
+          backgroundColor: "white",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          opacity: isOverscrolled ? 1: 0,
+          transform: isOverscrolled ? "scale(1)": "scale(0.8)",
+          transition: "all 0.5s ease-in-out",
+          position: "fixed",
+          bottom: 150,
+          left: 0,
+          right: 0,
+          zIndex: 10
+          }}
+        >
+          < img src = "/sunshine.png" className = "fixed bottom-0 left-0 h-96 z-10 pointer-events-none"
+            />
+        </div>
+    </div>
   );
 }
